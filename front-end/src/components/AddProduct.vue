14115897e1ebc38052f384d10bd1a390d6c70d70
Thiago Henrique Heinle Sanchez <thiagohenque@live.com>
2024-10-07 17:43:26 -0300

    testando o app

--- front-end/src/components/AddProduct.vue
+++ front-end/src/components/AddProduct.vue
@@ -1,13 +1,34 @@
 <template>
-  <h1> Adicionar produtos </h1>
+  <div>
+    <h2>Adicionar Produto</h2>
+    <form @submit.prevent="handleSubmit">
+      <input v-model="name" placeholder="Nome do produto" required />
+      <input v-model="price" type="number" placeholder="Preço" required />
+      <textarea v-model="description" placeholder="Descrição"></textarea>
+      <button type="submit">Adicionar</button>
+    </form>
+  </div>
 </template>
 
 <script>
+import { createProduct } from '../services/api';
+
 export default {
-    name: 'AddProduct'
-}
+  data() {
+    return {
+      name: '',
+      price: 0,
+      description: '',
+    };
+  },
+  methods: {
+    async handleSubmit() {
+      await createProduct(this.name, this.price, this.description);
+      this.name = '';
+      this.price = 0;
+      this.description = '';
+      this.$emit('product-added');
+    },
+  },
+};
 </script>
-
-<style>
-
-</style>
--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,16 +1,38 @@
-<script setup >
+<script  >
 import Navbar from '@/components/Navbar.vue';
 import AddProduct from '@/components/AddProduct.vue';
 import ProductList from '@/components/ProductList.vue';
+
+import { fetchProducts } from '../services/api';
+
+export default {
+  components: {
+    ProductList,
+    AddProduct,
+  },
+  data() {
+    return {
+      products: [],
+    };
+  },
+  methods: {
+    async fetchProducts() {
+      this.products = await fetchProducts();
+    },
+  },
+  created() {
+    this.fetchProducts();
+  },
+};
 
 </script>
 
 
 <template>
-    <div>
+    <div id="app" >
         <navbar />
-        <AddProduct />
-        <ProductList />
+        <AddProduct  @product-added="fetchProducts"  />
+        <ProductList :products="products" />
     </div>
 
 </template>
